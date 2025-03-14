9ee0c1a21a5d8839fa4a00c336087b6746006b0a
Sergho <sergche04@gmail.com>
2025-03-14 19:35:01 +0300

    feat: add global and module scss

--- src/renderer/components/App/App.tsx
+++ src/renderer/components/App/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './App.module.scss';
 
 const App: React.FC = () => {
   return (
-    <div>
+    <div className={styles.app}>
       <h1>Hello, Electron with React!</h1>
     </div>
   );
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,6 +1,7 @@
+import './global.scss';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './components/App/App';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
