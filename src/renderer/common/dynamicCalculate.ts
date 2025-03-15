59bf7b8e29454807f2cd58b8d7c461764d0cfd07
Sergho <sergche04@gmail.com>
2025-03-15 23:07:44 +0300

    feat: add other operation work via `.dll`

--- src/renderer/common/dynamicCalculate.ts
+++ src/renderer/common/dynamicCalculate.ts
@@ -4,7 +4,13 @@ export const dynamicCalculate = async (
   operands: string[],
   operation: string
 ): Promise<string> => {
-  const result = await axios.get('http://localhost:3001/sum', {
+  const urls = {
+    '+': 'http://localhost:3001/addition',
+    '-': 'http://localhost:3001/subtraction',
+    x: 'http://localhost:3001/multiplication',
+    '/': 'http://localhost:3001/division',
+  };
+  const result = await axios.get(urls[operation], {
     params: {
       first: +operands[0],
       second: +operands[1],
--- src/server/index.ts
+++ src/server/index.ts
@@ -13,6 +13,9 @@ app.use(
 
 const lib = Library(path.resolve(__dirname, '../../build/libNelderMead'), {
   NM_addition: ['double', ['double', 'double']],
+  NM_subtraction: ['double', ['double', 'double']],
+  NM_multiplication: ['double', ['double', 'double']],
+  NM_division: ['double', ['double', 'double']],
 });
 
 interface OperationDTO {
@@ -20,11 +23,38 @@ interface OperationDTO {
   second: string;
 }
 
-app.get('/sum', (req: Request<{}, {}, {}, OperationDTO>, res: Response<string>): void => {
-  const { first, second } = req.query;
-  let result = String(lib.NM_addition(+first, +second));
-  res.json(result);
-});
+app.get(
+  '/addition',
+  (req: Request<{}, {}, {}, OperationDTO>, res: Response<string>): void => {
+    const { first, second } = req.query;
+    let result = String(lib.NM_addition(+first, +second));
+    res.json(result);
+  }
+);
+app.get(
+  '/subtraction',
+  (req: Request<{}, {}, {}, OperationDTO>, res: Response<string>): void => {
+    const { first, second } = req.query;
+    let result = String(lib.NM_subtraction(+first, +second));
+    res.json(result);
+  }
+);
+app.get(
+  '/multiplication',
+  (req: Request<{}, {}, {}, OperationDTO>, res: Response<string>): void => {
+    const { first, second } = req.query;
+    let result = String(lib.NM_multiplication(+first, +second));
+    res.json(result);
+  }
+);
+app.get(
+  '/division',
+  (req: Request<{}, {}, {}, OperationDTO>, res: Response<string>): void => {
+    const { first, second } = req.query;
+    let result = String(lib.NM_division(+first, +second));
+    res.json(result);
+  }
+);
 
 app.listen(3001, () => {
   console.log(`Server is running on http://localhost:3001`);
