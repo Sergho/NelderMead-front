dbb77acaec13d6df84b48a8f485662f713a84504
Sergho <sergche04@gmail.com>
2025-04-04 14:17:18 +0300

    refactor: minify `server` index

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,24 +1,3 @@
-import express from 'express';
-import { CORS, ROUTES } from './config';
-import { OperationResponse } from './types/operation-response.type';
-import { OperationRequest } from './types/operation-request.type';
-import { BACKEND_PORT, HOST } from '../settings';
-
-export const startServer = () => {
-	const app = express();
-	app.use(CORS);
-
-	for (const route in ROUTES) {
-		app.get(route, (req: OperationRequest, res: OperationResponse): void => {
-			const { first, second } = req.query;
-			let result = String(ROUTES[route]([+first, +second]));
-			res.json(result);
-		});
-	}
-
-	app.listen(BACKEND_PORT, () => {
-		console.log(`Backend started: ${HOST}:${BACKEND_PORT}`);
-	});
-};
+import { startServer } from './utils/startServer';
 
 startServer();
---
+++ src/server/utils/startServer.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import { BACKEND_PORT, HOST } from '../../settings';
+import { CORS, ROUTES } from '../config';
+import { OperationRequest } from '../types/operation-request.type';
+import { OperationResponse } from '../types/operation-response.type';
+
+export const startServer = () => {
+  const app = express();
+  app.use(CORS);
+
+  for (const route in ROUTES) {
+    app.get(route, (req: OperationRequest, res: OperationResponse): void => {
+      const { first, second } = req.query;
+      let result = String(ROUTES[route]([+first, +second]));
+      res.json(result);
+    });
+  }
+
+  app.listen(BACKEND_PORT, () => {
+    console.log(`Backend started: ${HOST}:${BACKEND_PORT}`);
+  });
+};
