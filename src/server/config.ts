5049913e3c0f88f25a9d23ff101d1fc63dba3a0b
Sergho <sergche04@gmail.com>
2025-04-04 14:10:11 +0300

    fix: remove extra `dev` and `build` scripts

--- src/server/config.ts
+++ src/server/config.ts
@@ -4,23 +4,23 @@ import { HOST, FRONTEND_PORT } from '../settings';
 import { loadDLL } from './load-DLL';
 
 export const DLL_PATH =
-	process.env.NELDERMEAD_ENV === 'dev'
-		? path.resolve(__dirname, '../backend/libNelderMead.so')
-		: path.join(
-				__dirname,
-				'../../../',
-				'app.asar.unpacked',
-				'dist',
-				'backend',
-				'libNelderMead.so'
-		  );
+  process.env.NELDERMEAD_ENV === 'dev'
+    ? path.resolve(__dirname, '../../dist/backend/libNelderMead.so')
+    : path.join(
+        __dirname,
+        '../../../',
+        'app.asar.unpacked',
+        'dist',
+        'backend',
+        'libNelderMead.so'
+      );
 export const CORS = cors({
-	origin: `${HOST}:${FRONTEND_PORT}`,
+  origin: `${HOST}:${FRONTEND_PORT}`,
 });
 export const LIB = loadDLL(DLL_PATH, 'calculator');
 export const ROUTES = {
-	'/addition': LIB.NM_addition,
-	'/subtraction': LIB.NM_subtraction,
-	'/multiplication': LIB.NM_multiplication,
-	'/division': LIB.NM_division,
+  '/addition': LIB.NM_addition,
+  '/subtraction': LIB.NM_subtraction,
+  '/multiplication': LIB.NM_multiplication,
+  '/division': LIB.NM_division,
 };
