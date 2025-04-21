ab224884f45e73fd1b32105eac93445fb4621679
Sergho <sergche04@gmail.com>
2025-04-21 12:31:36 +0300

    feat: auto row index length

--- src/client/components/Log/ui/Text/Text.tsx
+++ src/client/components/Log/ui/Text/Text.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx';
 import classes from './Text.module.scss';
-import { FC } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 import { Row } from '../Row/Row';
 
 interface TextProps {
@@ -10,17 +10,33 @@ interface TextProps {
 
 export const Text: FC<TextProps> = (props: TextProps) => {
   const { className, content } = props;
+  const rows = content.length === 0 ? [] : content.split('\n');
+  const indexRefs = useRef<(HTMLSpanElement | null)[]>([]);
+  const [indexWidth, setIndexWidth] = useState(0);
 
-  const getRows = (): string[] => {
-    if (content.length === 0) return [];
-    return content.split('\n');
-  };
+  useEffect(() => {
+    const widths = indexRefs.current
+      .filter((ref) => ref !== null)
+      .map((ref) => ref?.offsetWidth || 0);
+
+    console.log(widths);
+
+    const maxWidth = Math.max(...widths, 0);
+    setIndexWidth(maxWidth);
+  }, [content]);
 
   return (
     <div className={clsx(className, classes.wrapper)}>
-      {getRows().map((row: string, index: number) => {
+      {rows.map((row: string, index: number) => {
         return (
-          <Row key={index} index={index}>
+          <Row
+            key={index}
+            index={index}
+            indexWidth={indexWidth}
+            ref={(el) => {
+              indexRefs.current[index] = el;
+            }}
+          >
             {row}
           </Row>
         );
