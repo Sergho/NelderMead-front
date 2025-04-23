889981e1aee9b75452effedbb46f02d4e8c8f06d
Sergho <sergche04@gmail.com>
2025-04-23 23:01:48 +0300

    feat: add server error handling

--- src/client/axios/create-tree.ts
+++ src/client/axios/create-tree.ts
@@ -2,8 +2,12 @@ import axios from 'axios';
 import { API } from '../constants';
 
 export const createTree = async (expression: string) => {
-  const result = await axios.get(API.create_tree, {
-    params: { expression },
-  });
-  return result.data;
+  try {
+    const result = await axios.get(API.create_tree, {
+      params: { expression },
+    });
+    return result.data;
+  } catch (err) {
+    return { ...err.response.data, status: err.response.status };
+  }
 };
