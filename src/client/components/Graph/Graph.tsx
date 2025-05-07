f19181557104e68da4070666cce59d06153a04bb
Sergho <sergche04@gmail.com>
2025-05-07 22:16:38 +0300

    feat: add simple 2D graph plug

--- src/client/components/Graph/Graph.tsx
+++ src/client/components/Graph/Graph.tsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx';
 import classes from './Graph.module.scss';
 import { FC } from 'react';
+import Plot from 'react-plotly.js';
 
 interface GraphProps {
   className?: string;
@@ -8,5 +9,27 @@ interface GraphProps {
 
 export const Graph: FC<GraphProps> = (props: GraphProps) => {
   const { className } = props;
-  return <div className={clsx(className, classes.wrapper)}></div>;
+  return (
+    <div className={clsx(className, classes.wrapper)}>
+      <Plot
+        className={clsx(classes.plot)}
+        data={[
+          {
+            x: [1, 2, 3],
+            y: [2, 6, 3],
+            type: 'scatter',
+            mode: 'lines+markers',
+          },
+        ]}
+        layout={{
+          dragmode: 'pan',
+          autosize: true,
+          paper_bgcolor: 'rgba(0,0,0,0)',
+          plot_bgcolor: 'rgba(0,0,0,0)',
+          xaxis: { linecolor: 'black', gridcolor: 'rgba(200,200,200,0.5)' },
+          yaxis: { linecolor: 'black', gridcolor: 'rgba(200,200,200,0.5)' },
+        }}
+      />
+    </div>
+  );
 };
